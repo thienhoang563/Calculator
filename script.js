5c6b3c6033bd0fbd4f25e3061d506b3b6fba8f98
thienhoang563 <thienhoang563@gmail.com>
2018-12-14 16:17:32 +0700

    Add README.md file

---
+++ script.js
@@ -0,0 +1,24 @@
+function getValueA() {
+   return document.getElementById('numA').value;
+}
+function getValueB() {
+   return document.getElementById('numB').value;
+}
+function setResult(valueAsign) {
+    document.getElementById('result').value = valueAsign;
+}
+function add(numA, numB) {
+    return parseFloat(numA) + parseFloat(numB);
+}
+function sub(numA, numB) {
+    return parseFloat(numA) - parseFloat(numB);
+}
+function mul(numA, numB) {
+    return parseFloat(numA) * parseFloat(numB);
+}
+function div(numA, numB) {
+    return parseFloat(numA) / parseFloat(numB);
+}
+function mod(numA, numB) {
+    return parseFloat(numA) % parseFloat(numB);
+}
